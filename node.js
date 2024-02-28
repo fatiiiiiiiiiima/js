e608223a9963fa5ce8991d72d9f85e6ad3d1abb1
fatiiiiiiiiiima <160717230+fatiiiiiiiiiima@users.noreply.github.com>
2024-02-28 16:24:50 +0500

    first commit

---
+++ node.js
@@ -0,0 +1,19 @@
+const accountId = 1332
+let accountEmail = "fatima@gmail.com"
+var accountPassword = "123"
+accountCity = "lahore"
+//  accountId = 2 not allowed
+  console.log(accountId);
+console.table([accountEmail,accountId,accountCity,accountPassword]);
+
+// data types in js
+// number
+// BigInt
+// Boolean
+// null
+// undefined
+
+// object is unprimitive data types 
+// null is primitive
+// null is an object 
+// undefined is data type 
